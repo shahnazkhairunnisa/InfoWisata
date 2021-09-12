7512c44736c3d330bd7349c66aa19b50a24d9df2
shahnazkhairunnisa <tiarakhusnulkhotimah017@gmail.com>
2021-09-12 11:57:34 +0700

    coba post android studio

---
+++ app/src/main/java/com/capstone/infowisata/Api/Api.java
@@ -0,0 +1,16 @@
+package com.capstone.infowisata.Api;
+
+/**
+ * Created by Naz on 22-12-2020.
+ */
+
+public class Api {
+
+    public static String Wisata = "https://dev.farizdotid.com/api/purwakarta/wisata";
+    public static String DetailWisata = "https://dev.farizdotid.com/api/purwakarta/wisata/{id}";
+    /*public static String Komunitas = "https://dev.farizdotid.com/api/purwakarta/komunitas";
+    public static String DetailKomunitas = "https://dev.farizdotid.com/api/purwakarta/komunitas/{id}";
+    public static String KategoriKomunitas = "https://dev.farizdotid.com/api/purwakarta/komunitas?kategori={nama_kategori}";
+    public static String RuteAngkot = "https://dev.farizdotid.com/api/purwakarta/ruteangkot";*/
+
+}
